488a68ce3e50c8ef10d7e770682541feb430a344
EL Marsho <emarsho@gmail.com>
2024-10-26 02:19:27 -0500

    add hero section html

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,12 @@ const Hero = () => {
     return (
         <section className='hero-section'>
             <h1 className='hero-header'>Nice to meet you! I'm <span className='underline'>Adam Keyes</span>.</h1>
-            <p className='hero-text'>
-                Based in the UK, I'm a front-end developer passionate about
-                building accessible web apps that users love.
-            </p>
+            <div className='hero-text'>
+                <p>
+                    Based in the UK, I'm a front-end developer passionate about
+                    building accessible web apps that users love.
+                </p>
+            </div>
             <div className='btn-container'>
                 <button className='hero-button' type='button'><span className='btn-underline'>CONTACT ME</span></button>
                 <image src='/public/images/pattern-circle.svg' alt='circle graphic' className='circle-graphic'/>
