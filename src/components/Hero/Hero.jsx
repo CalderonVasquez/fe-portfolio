7f71540f908aa15992f91ddb0d6529094f6c9497
EL Marsho <emarsho@gmail.com>
2024-10-30 21:06:51 -0500

    fix hero-header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css';
 const Hero = () => {
     return (
         <section className='hero-section'>
-            <h1 className='hero-header'>Nice to<br className='tablet-break'></br>meet you! I'm <span className='underline'>Adam Keyes</span>.</h1>      
+            <h1 className='hero-header'>Nice to <br className='tablet-break'></br>meet you! I'm <span className='underline'>Adam Keyes</span>.</h1>      
             <p className='hero-text'>
                 Based in the UK, I'm a front-end developer passionate about
                 building accessible web apps that users love.
